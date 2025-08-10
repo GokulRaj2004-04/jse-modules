3d773dc1c3d91e8e6cbaf4474aa0707d3634a304
Gokul Raj V <gokul20041104@gmail.com>
2025-08-10 15:41:01 +0530

    Delete week3 directory

--- week3/librarymanagement4/BookService.java
+++
@@ -1,7 +0,0 @@
-package com.library.service;
-
-public class BookService {
-    public void serve() {
-        System.out.println("Serving book...");
-    }
-}
--- week3/librarymanagement4/MainApp.java
+++
@@ -1,7 +0,0 @@
-package com.library;
-
-public class MainApp {
-    public static void main(String[] args) {
-        System.out.println("Library Management App Running");
-    }
-}
